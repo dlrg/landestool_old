410a96448aab8c735994ace427e23f54858fec56
Barne Jürgensen <barne.juergensen@codeanker.de>
2017-11-18 17:20:55 +0100

    fix tests

--- server/test/services/program.test.js
+++ server/test/services/program.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const app = require('../../server/app')
+const app = require('../../app')
 
 describe('\'program\' service', () => {
   it('registered the service', () => {
