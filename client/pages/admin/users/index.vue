a55c1814b865f62d423c33eaee983e5e22bbd757
Katharina Rohwer <katharina.rohwer@codeanker.de>
2018-03-14 15:01:12 +0100

    UserFilter eingebaut, Shirt Bestell Seite

--- client/pages/admin/users/index.vue
+++ client/pages/admin/users/index.vue
@@ -10,6 +10,15 @@
                             <button class="btn btn-sm btn-info c-white float-right" @click="add">Hinzufügen</button>
                         </div>
                         <div class="card-body">
+                          <div class="select-wrapper">
+                            <select class="form-control  float-left" name="selectFormUser" id="selectFormEinsatz" v-model="userFilter">
+                              <option value="alle">alle</option>
+                              <option value="kein Ausweis / Einsetzbar">kein Ausweis / Einsetzbar</option>
+                              <option value="Ausweis / nicht Einsetzbar bzw. anderweitig verplant">Ausweis / nicht Einsetzbar bzw. anderweitig verplant</option>
+                              <option value="kein Ausweis / nicht Einsetzbar bzw. anderweitig verplant">kein Ausweis / nicht Einsetzbar bzw. anderweitig verplant</option>
+                              <option value="Arbeitsgruppe">Arbeitsgruppe </option>
+                            </select>
+                           </div>
                             <table class="table table-striped">
                                 <thead>
                                 <tr >
@@ -21,7 +30,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                    <nuxt-link tag="tr" :to="item._id" append v-for="item in users" :key="item._id">
+                                    <nuxt-link tag="tr" :to="item._id" append v-for="item in filteredUsers" :key="item._id">
                                         <td>{{ item.firstname }}</td>
                                         <td>{{ item.lastname}}</td>
                                         <td>{{ item.email }}</td>
@@ -41,13 +50,26 @@
 <script>
   import { mapGetters } from 'vuex'
   import NuxtLink from '@/../.nuxt/components/nuxt-link'
-
+  
   export default {
     components: {NuxtLink},
     computed: {
       ...mapGetters({
         users: 'user/list'
-      })
+      }),
+      filteredUsers () {
+        if (this.userFilter === 'alle') {
+          return this.users
+        } else {
+          return this.users.filter(user => user.presence === this.userFilter)
+        }
+      }
+    },
+    data () {
+      return {
+        deleteConfirm: false,
+        userFilter: 'alle'
+      }
     },
     methods: {
       add () {
@@ -55,11 +77,7 @@
       },
       async remove (_id) {
         await this.$store.dispatch('user/remove', _id)
-        alert('Benutzer geöscht')
       }
-    },
-    showAlert: () => {
-      alert('Löschen erfolgreich')
     },
     async fetch ({ store }) {
       await store.dispatch('user/find')
