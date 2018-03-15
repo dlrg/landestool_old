2f8556fbff3086f0be333b9ca939e9c20d61330d
Katharina Rohwer <katharina.rohwer@codeanker.de>
2018-03-15 16:01:13 +0100

    dayrow funktioniert, Array in DB

--- client/pages/admin/users/_userId/timetableEdit.vue
+++ client/pages/admin/users/_userId/timetableEdit.vue
@@ -14,14 +14,41 @@
                         <div class="card-body">
                             <div class="form-group row">
                                 <h4 class="col-4">Tag</h4>
-                                <h4 class="col-4">von</h4>
-                                <h4 class="col-4">bis</h4>
+                                <h4 class="col-3">von</h4>
+                                <h4 class="col-3">bis</h4>
+                                <div class="col-2"></div>
                             </div>
-                            <div class="form-group row">
-                                <dayrow v-for="n in range">
-                                </dayrow>
-                                <button type="button" v-on:click="addForm()">Add Form</button>
+                            <div class="form-group row" v-for="(item, index) in user.time" :key="'time'+index">
+                                <div class="select-wrapper col-4 mb-3">
+                                    <select class="form-control  float-left" id="timeDay" v-model="item.day">
+                                        <option value="Donnerstag">Donnerstag</option>
+                                        <option value="Freitag">Freitag</option>
+                                        <option value="Samstag">Samstag</option>
+                                        <option value="Sonntag">Sonntag</option>
+                                    </select>
+                                </div>
+                                <div class="select-wrapper col-xl-3 mb-3">
+                                    <select class="form-control  float-left" id="timeStart" v-model="item.start">
+                                        <option value="14:00">14:00</option>
+                                        <option value="15:00">15:00</option>
+                                        <option value="16:00">16:00</option>
+                                        <option value="17:00">17:00</option>
+                                    </select>
+                                </div>
+                                <div class="select-wrapper col-xl-3 mb-3">
+                                    <select class="form-control  float-left" id="timeEnd" v-model="item.end">
+                                        <option value="14:00">14:00</option>
+                                        <option value="15:00">15:00</option>
+                                        <option value="16:00">16:00</option>
+                                        <option value="17:00">17:00</option>
+                                    </select>
+                                </div>
+                                <div class="col-xl-1"></div>
+                                <div class="delete-button col-xl-1">
+                                  <button class="btn btn-sm btn-danger float-right" @click.stop="remove(index)">Löschen</button>
+                                </div>
                             </div>
+                            <button type="button" v-on:click="addForm()">Add Form</button>
                          </div>
                       </div>
                     </div>
@@ -33,54 +60,25 @@
 
 <script>
   export default {
-    data: {
-  	  range:0
-    },
     computed: {
       user () {
         return this.$store.getters['user/get'](this.$route.params.userId)
       }
-    },
-    components: {
-      dayrow
     },
     methods: {
       save () {
         this.$store.dispatch('user/patch', [this.user._id, this.user])
         this.$router.push('/admin/users/' + this.user._id + '/timetable')
       },
+      remove (index) {
+        this.user.time.splice(index, 1)
+      },
       addForm () {
-        this.range += 1
+        this.user.time.push({})
       }
     },
     async fetch ({ store, params }) {
       await store.dispatch('user/get', params.userId)
     }
   }
-</script>
-<script type="x-template" id="dayrow">
-  <div class="select-wrapper col-4 mb-3">
-    <select class="form-control  float-left" name="selectFormUser" id="timeDay" v-model="user.time.day">
-        <option value="Donnerstag">Donnerstag</option>
-        <option value="Freitag">Freitag</option>
-        <option value="Samstag">Samstag</option>
-        <option value="Sonntag">Sonntag</option>
-    </select>
-</div>
-<div class="select-wrapper col-xl-4 mb-3">
-    <select class="form-control  float-left" name="selectFormUser" id="timeStart" v-model="user.time.start">
-        <option value="14:00">14:00</option>
-        <option value="15:00">15:00</option>
-        <option value="16:00">16:00</option>
-        <option value="17:00">17:00</option>
-    </select>
-</div>
-<div class="select-wrapper col-xl-4 mb-3">
-    <select class="form-control  float-left" name="selectFormUser" id="timeEnd" v-model="user.time.end">
-        <option value="14:00">14:00</option>
-        <option value="15:00">15:00</option>
-        <option value="16:00">16:00</option>
-        <option value="17:00">17:00</option>
-    </select>
-</div>   
 </script>
